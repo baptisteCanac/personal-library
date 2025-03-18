e183952f0a4d843ad179e8d7a3c549c6fc406536
Baptiste <74823118+baptisteCanac@users.noreply.github.com>
2025-03-18 18:00:48 +0100

    securisation library book remove

--- library-manager.py
+++ library-manager.py
@@ -34,18 +34,24 @@ class LibraryManager:
 
         self.library.append(new_book)
         self.save_library()
+        print("-----------------")
         print(f"Book {title} added successfully.")
+        print("-----------------")
     
     def remove_book(self):
         title = input("Enter the title book to remove from the library ")
         initial_length = len(self.library)
-        self.library = [book for book in self.library if book['title'].lower() != title]
+        self.library[:] = [book for book in self.library if book['title'].lower() != title]
 
         if len(self.library) < initial_length:
             self.save_library()
+            print("-----------------")
             print(f"Book {title} removed successfully.")
+            print("-----------------")
         else: 
+            print("-----------------")
             print(f"Book {title} not found in the library.")
+            print("-----------------")
     
     def search_library(self):
         search_by = input("Search by title or author ").lower()
@@ -58,7 +64,9 @@ class LibraryManager:
                 status = "Read" if book['read'] else 'Unread'
                 print(f"{book['title']} by {book['author']} - {book['year']} - {book['genre']} - {status}")
         else:
+            print("-----------------")
             print(f"No books found matching '{search_term}' in the {search_by} field.")
+            print("-----------------")
     
     def display_all_books(self):
         if self.library:
@@ -66,20 +74,21 @@ class LibraryManager:
                 status = "Read" if book['read'] else 'Unread'
                 print(f"{book['title']} by {book['author']} - {book['year']} - {book['genre']} - {status}")
         else:
+            print("-----------------")
             print("The library is empty.")
+            print("-----------------")
     
     def display_statistics(self):
         total_books = len(self.library)
         read_books = len([book for book in self.library if book['read']])
         percentage_read = (read_books / total_books) * 100 if total_books > 0 else 0
 
+        print("-----------------")
         print(f"Total books: {total_books}")
         print(f"Percentage read: {percentage_read:.2f}%")
-
-if __name__ == '__main__':
-    libraryManager = LibraryManager()
-
-    while True:
+        print("-----------------")
+    
+    def display_menu(self):
         print("Welcome to the library manager.")
         print("1. Add a book")
         print("2. Remove a book")
@@ -88,7 +97,14 @@ if __name__ == '__main__':
         print("5. Display statistics")
         print("6. Exit")
 
+if __name__ == '__main__':
+    libraryManager = LibraryManager()
+
+    while True:
+        libraryManager.display_menu()
+
         choice = input("Enter your choice: ")
+       
         if choice == '1':
             libraryManager.add_book()
         elif choice == '2':
